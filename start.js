8aa890960ee023655487886b4a3eebd4f9e770ad
Pablo Molina <contact@pablomolina.me>
2019-11-22 20:44:27 +0100

    Remove browser-sync and related a create a raw node http server

--- start.js
+++ start.js
@@ -1,19 +1,43 @@
-const browserSync = require('browser-sync');
-const enquirer = require('enquirer');
-const glob = require('glob');
+const fs = require('fs');
+const http = require('http');
+const path = require('path');
 
-new enquirer.Select({
-  name: 'project',
-  message: 'Select a project',
-  choices: glob
-    .sync('*/index.html')
-    .map((pathName) => pathName.replace('/index.html', '')),
-})
-  .run()
-  .then((project) => {
-    const bs = browserSync.create();
-    bs.init({
-      files: [`${project}/**/*`],
-      server: `./${project}`,
-    });
-  });
+const SERVER_PORT = 3000;
+const SERVER_PATH = path.join(__dirname, process.argv[2]);
+
+const EXTENSION_TO_CONTENT_TYPE = {
+  css: 'text/css',
+  html: 'text/html',
+  jpg: 'image/jpg',
+  js: 'text/javascript',
+  json: 'application/json',
+  png: 'image/png',
+};
+
+const extensionToContentType = (extension) => {
+  return EXTENSION_TO_CONTENT_TYPE[extension.substr(1)] || 'text/plain';
+};
+
+const requestHandler = ({ url }, response) => {
+  console.log(url);
+
+  const requestPath = url.substr(-1) === '/' ? `${url}index.html` : url;
+  const filePath = path.join(SERVER_PATH, requestPath);
+  const isFile = fs.existsSync(filePath) && fs.statSync(filePath).isFile();
+
+  if (isFile) {
+    const content = fs.readFileSync(filePath);
+    const contentType = extensionToContentType(path.extname(filePath));
+    response.writeHead(200, { 'Content-Type': contentType });
+    response.end(content);
+  } else {
+    response.writeHead(404);
+    response.end('Not found');
+  }
+};
+
+http.createServer(requestHandler).listen(SERVER_PORT, (error) => {
+  if (error) return console.err(error);
+  console.log(`Server listening on ${SERVER_PORT}`);
+  console.log(`Server path is ${SERVER_PATH}`);
+});
