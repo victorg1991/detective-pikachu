354c811bc45f0d4ae6928676a67f30a3c8098f6e
Pablo Molina <contact@pablomolina.me>
2019-11-23 11:53:01 +0100

    11-serve-from-cache

--- predictable-service-worker/sw.js
+++ predictable-service-worker/sw.js
@@ -1,9 +1,12 @@
 importScripts('/navigation-data.js');
 
+const CACHE_ID = 'predictable-service-worker-1';
+
 self.addEventListener('install', (event) => {
   event.waitUntil(
     (async () => {
       await self.skipWaiting();
+      await caches.delete(CACHE_ID);
       console.log('SW installed');
     })(),
   );
@@ -12,4 +15,25 @@ self.addEventListener('install', (event) => {
 self.addEventListener('activate', () => {
   self.clients.claim();
   console.log('SW activated');
+});
+
+self.addEventListener('fetch', (event) => {
+  const { pathname } = new URL(event.request.url);
+
+  if (Object.values(PAGES).includes(pathname)) {
+    event.respondWith(
+      (async () => {
+        const cache = await caches.open(CACHE_ID);
+        const cachedResponse = await cache.match(event.request);
+
+        if (cachedResponse) {
+          console.log(pathname, 'from cache');
+        } else {
+          console.log(pathname, 'from server');
+        }
+
+        return cachedResponse || fetch(event.request);
+      })(),
+    );
+  }
 });
