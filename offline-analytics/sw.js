a95d96cddec12d290d3c9c086189a40f34017e52
Pablo Molina <contact@pablomolina.me>
2019-11-23 11:14:17 +0100

    08-cache-application

--- offline-analytics/sw.js
+++ offline-analytics/sw.js
@@ -1,8 +1,14 @@
 importScripts('/idb.js');
 
+const CACHE_ID = 'offline-analytics-1';
+const CACHED_ASSETS = ['/', '/bulma.min.css', '/idb.js', '/sw.js'];
+
 self.addEventListener('install', (event) => {
   event.waitUntil(
     (async () => {
+      await caches.delete(CACHE_ID);
+      const cache = await caches.open(CACHE_ID);
+      await cache.addAll(CACHED_ASSETS);
       await self.skipWaiting();
       console.log('SW installed');
     })(),
@@ -16,4 +22,14 @@ self.addEventListener('activate', () => {
 
 self.addEventListener('message', (event) => {
   console.log(event.data);
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    (async () => {
+      const cache = await caches.open(CACHE_ID);
+      const cachedResponse = await cache.match(event.request);
+      return cachedResponse || fetch(event.request);
+    })(),
+  );
 });
