ff53603364cde017f2b4e2d25962c6adf63b7bcd
Pablo Molina <contact@pablomolina.me>
2019-11-23 11:23:48 +0100

    09-store-event

--- offline-analytics/sw.js
+++ offline-analytics/sw.js
@@ -1,7 +1,15 @@
 importScripts('/idb.js');
 
+const DB_ID = 'offline-analytics';
+const DB_VERSION = 1;
 const CACHE_ID = 'offline-analytics-1';
 const CACHED_ASSETS = ['/', '/bulma.min.css', '/idb.js', '/sw.js'];
+
+function openDatabase() {
+  return idb.open(DB_ID, DB_VERSION, (db) => {
+    db.createObjectStore('events', { autoIncrement: true });
+  });
+}
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
@@ -9,6 +17,7 @@ self.addEventListener('install', (event) => {
       await caches.delete(CACHE_ID);
       const cache = await caches.open(CACHE_ID);
       await cache.addAll(CACHED_ASSETS);
+      await idb.deleteDatabase(DB_ID);
       await self.skipWaiting();
       console.log('SW installed');
     })(),
@@ -20,7 +29,13 @@ self.addEventListener('activate', () => {
   console.log('SW activated');
 });
 
-self.addEventListener('message', (event) => {
+self.addEventListener('message', async (event) => {
+  if (event.data && event.data.name) {
+    const db = await openDatabase();
+    await idb.add(db, 'events', event.data);
+    db.close();
+  }
+
   console.log(event.data);
 });
 
